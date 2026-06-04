5687478aa89f7c5e353d43ca3c417411c2c3900e
Mário Cao <kreativermario@gmail.com>
2026-06-04 22:01:25 +0100

    fix: fix prisma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,12 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    settings: {
+      react: { version: "19" },
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
